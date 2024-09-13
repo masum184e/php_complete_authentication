12e4b7dd6b5578f63b1c4302f41f3a27d4409855
masum184e <masum184e@gmail.com>
2024-09-13 19:46:37 +0600

    db connection added

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost'; 
+$dbname = 'php_complete_authentication'; 
+$username = 'root'; 
+$password = ''; 
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
